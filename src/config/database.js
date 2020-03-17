cdcfceac1e860c602bbfea85d018ce825633fef6
Lucas Sônego <lucassonego@ufpr.br>
2020-03-16 21:00:33 -0300

    refactor: campo DB_DIALECT

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,7 @@
 require("dotenv/config");
 
 module.exports = {
-  dialect: "postgres",
+  dialect: process.env.DB_DIALECT,
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   username: process.env.DB_USERNAME,
