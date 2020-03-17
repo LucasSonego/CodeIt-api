c00623b92b79d27edd3850909d2c1cdc4871a1de
Lucas Sônego <lucassonego@ufpr.br>
2020-03-16 22:08:14 -0300

    feat: middleware para verificação de autenticação

--- src/routes.js
+++ src/routes.js
@@ -3,9 +3,19 @@ import { Router } from "express";
 import UserController from "./app/controllers/UserController";
 import SessionController from "./app/controllers/SessionController";
 
+import authMiddleware from "./app/middlewares/authMiddleware";
+
 const routes = Router();
 
 routes.post("/users", UserController.store);
 routes.post("/sessions", SessionController.store);
+
+routes.use(authMiddleware);
+
+routes.get("/tests", (req, res) => {
+  res.json({
+    id: req.userId,
+  });
+});
 
 export default routes;
