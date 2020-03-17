47e8d9b3202454a7330bb2fc1a997e20a69ab6a1
Lucas Sônego <lucassonego@ufpr.br>
2020-03-17 17:43:59 -0300

    test: framework e configurações para testes

--- src/config/database.js
+++ src/config/database.js
@@ -1,15 +1,27 @@
 require("dotenv/config");
 
-module.exports = {
-  dialect: process.env.DB_DIALECT,
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
-  define: {
-    timestamps: true,
-    underscored: true,
-    underscoredAll: true,
-  },
-};
+if (process.env.NODE_ENV === "test") {
+  module.exports = {
+    dialect: "sqlite",
+    storage: "./__tests__/database.sqlite",
+    define: {
+      timestamps: true,
+      underscored: true,
+      underscoredAll: true,
+    },
+  };
+} else {
+  module.exports = {
+    dialect: process.env.DB_DIALECT,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    define: {
+      timestamps: true,
+      underscored: true,
+      underscoredAll: true,
+    },
+  };
+}
