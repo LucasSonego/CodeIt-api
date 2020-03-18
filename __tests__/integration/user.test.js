97805edb95a50ce773dd9748e3035348164b5d54
Lucas Sônego <lucassonego@ufpr.br>
2020-03-18 00:03:40 -0300

    feat: validação dos campos das requisições de cadastro de usuario

--- __tests__/integration/user.test.js
+++ __tests__/integration/user.test.js
@@ -7,7 +7,7 @@ describe("Teste dos sistemas de Usuario e Sessão", () => {
    */
   let token;
 
-  it("Cadastrar um novo usuario", async () => {
+  test("Cadastrar um novo usuario", async () => {
     const response = await request(app)
       .post("/users")
       .send({
@@ -19,7 +19,39 @@ describe("Teste dos sistemas de Usuario e Sessão", () => {
     expect(response.body).toHaveProperty("id", "name", "email", "is_teacher");
   });
 
-  it("Fazer login", async () => {
+  test("Validação dos campos do corpo da requisição de criação de usuario", async () => {
+    const response = await request(app)
+      .post("/users")
+      .send({
+        name: "Test User",
+        password: "123456",
+      });
+
+    expect(response.body).toHaveProperty("error");
+  });
+
+  test("Impedir que um usuario se cadastre com um email já cadastrado", async () => {
+    await request(app)
+      .post("/users")
+      .send({
+        name: "Test User",
+        email: "testmail@gmail.com",
+        password: "123456",
+        is_teacher: true,
+      });
+
+    const response = await request(app)
+      .post("/users")
+      .send({
+        name: "Test User 2",
+        email: "testmail@gmail.com",
+        password: "654321",
+      });
+
+    expect(response.body).toHaveProperty("error");
+  });
+
+  test("Fazer login", async () => {
     const response = await request(app)
       .post("/sessions")
       .send({
@@ -32,7 +64,7 @@ describe("Teste dos sistemas de Usuario e Sessão", () => {
     token = response.body.token;
   });
 
-  it("Impedir login quando os dados enviados são invalidos", async () => {
+  test("Impedir login quando os dados enviados são invalidos", async () => {
     const response = await request(app)
       .post("/sessions")
       .send({
@@ -43,7 +75,7 @@ describe("Teste dos sistemas de Usuario e Sessão", () => {
     expect(response.body).toHaveProperty("error");
   });
 
-  it("Validação de autenticação (token correto)", async () => {
+  test("Validação de autenticação (token correto)", async () => {
     const response = await request(app)
       .get("/tests")
       .set("Authorization", "Bearer " + token);
@@ -51,7 +83,7 @@ describe("Teste dos sistemas de Usuario e Sessão", () => {
     expect(response.body).toHaveProperty("id");
   });
 
-  it("Validação de autenticação (token invalido)", async () => {
+  test("Validação de autenticação (token invalido)", async () => {
     const response = await request(app)
       .get("/tests")
       .set("Authorization", "Bearer " + "~token invalido~");
@@ -59,7 +91,7 @@ describe("Teste dos sistemas de Usuario e Sessão", () => {
     expect(response.body).toHaveProperty("error");
   });
 
-  it("Validação de autenticação (sem token)", async () => {
+  test("Validação de autenticação (sem token)", async () => {
     const response = await request(app).get("/tests");
 
     expect(response.body).toHaveProperty("error");
--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,7 +1,36 @@
+import * as yup from "yup";
+
 import User from "../models/User";
 
 class UserController {
   async store(req, res) {
+    const schema = yup.object().shape({
+      name: yup.string().required(),
+      email: yup
+        .string()
+        .required()
+        .email(),
+      password: yup
+        .string()
+        .required()
+        .min(6),
+      is_teacher: yup.boolean(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({
+        error: "Um ou mias campos obrigatorios não foram preenchidos",
+      });
+    }
+
+    const emailAlreadyUsed = await User.findOne({
+      where: { email: req.body.email },
+    });
+    if (emailAlreadyUsed) {
+      return res.status(409).json({
+        error: "Este email já esta cadastrado para outro usuario",
+      });
+    }
     const { id, name, email, is_teacher } = await User.create(req.body);
 
     return res.send({
