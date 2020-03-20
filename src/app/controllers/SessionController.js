cdd2704d92f85677624456bdab9a36346e0a7203
Lucas Sônego <lucassonego@ufpr.br>
2020-03-19 23:08:54 -0300

    :sparkles: feat: Validação dos campos das requisições de login

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -1,15 +1,29 @@
 import jwt from "jsonwebtoken";
+import * as yup from "yup";
 
 import User from "../models/User";
 import authConfig from "../../config/auth";
 
 class SessionController {
   async store(req, res) {
+    const schema = yup.object().shape({
+      email: yup
+        .string()
+        .email()
+        .required(),
+      password: yup.string().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({
+        error: "Um ou mais campos não foram preenchidos corretamente",
+      });
+    }
     const { email, password } = req.body;
     const user = await User.findOne({ where: { email } });
     if (!user || !(await user.checkPassword(password))) {
       return res.status(401).json({
-        error: "usuario ou senha inválidos",
+        error: "Usuario ou senha inválidos",
       });
     }
 
