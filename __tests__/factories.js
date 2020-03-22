edf3773ea5d613743bda0a913bdca733442b044f
Lucas Sônego <lucassonego@ufpr.br>
2020-03-22 19:41:50 -0300

    :bug: fix: problema onde a factory estava gerando sempre usuarios iguais

--- __tests__/factories.js
+++ __tests__/factories.js
@@ -2,11 +2,11 @@ import faker from "faker";
 import { factory } from "factory-girl";
 import User from "../src/app/models/User";
 
-factory.define("User", User, {
+factory.define("User", User, () => ({
   name: faker.name.findName(),
   email: faker.internet.email(),
   password: faker.internet.password(),
   is_teacher: faker.random.boolean(),
-});
+}));
 
 export default factory;
