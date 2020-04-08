426d7046f559e3566845401f6379a2f7894361ef
Lucas Sônego <lucassonego@ufpr.br>
2020-04-08 15:18:45 -0300

    :sparkles: feat: Tabela no banco de dados, model e relações

--- src/database/index.js
+++ src/database/index.js
@@ -3,8 +3,9 @@ import Sequelize from "sequelize";
 import databaseConfig from "../config/database";
 
 import User from "../app/models/User";
+import Discipline from "../app/models/Discipline";
 
-const models = [User];
+const models = [User, Discipline];
 
 class Database {
   constructor() {
@@ -15,6 +16,9 @@ class Database {
     this.connection = new Sequelize(databaseConfig);
 
     models.map(model => model.init(this.connection));
+    models.map(
+      model => model.associate && model.associate(this.connection.models)
+    );
   }
 }
 
