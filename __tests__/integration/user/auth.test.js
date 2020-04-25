a031436bcd13c450398dafae4b9e2b80ace8fe9c
Lucas Sônego <lucassonego@ufpr.br>
2020-04-24 22:29:30 -0300

    :sparkles: feat: Validação de autenticação pode retornar um novo token

--- __tests__/integration/user/auth.test.js
+++ __tests__/integration/user/auth.test.js
@@ -27,10 +27,26 @@ describe("Testes de validação de autenticação", () => {
       .set("Authorization", "Bearer " + token);
 
     expect(response.body).not.toHaveProperty("error");
-    expect(response.body.id).toBe(user.id);
-    expect(response.body.name).toBe(user.name);
-    expect(response.body.email).toBe(user.email);
-    expect(response.body.is_teacher).toBe(user.is_teacher);
+    expect(response.body).toHaveProperty("user");
+    expect(response.body.user.id).toBe(user.id);
+    expect(response.body.user.name).toBe(user.name);
+    expect(response.body.user.email).toBe(user.email);
+    expect(response.body.user.is_teacher).toBe(user.is_teacher);
+  });
+
+  test("Validação de autenticação (incluir novo token)", async () => {
+    const response = await request(app)
+      .get("/sessions")
+      .query({ newtoken: true })
+      .set("Authorization", "Bearer " + token);
+
+    expect(response.body).not.toHaveProperty("error");
+    expect(response.body).toHaveProperty("user");
+    expect(response.body).toHaveProperty("token");
+    expect(response.body.user.id).toBe(user.id);
+    expect(response.body.user.name).toBe(user.name);
+    expect(response.body.user.email).toBe(user.email);
+    expect(response.body.user.is_teacher).toBe(user.is_teacher);
   });
 
   test("Validação de autenticação (token invalido)", async () => {
